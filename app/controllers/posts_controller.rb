d69bb25c083a37bb5f9cf92a6ad6e3efd5deb1e5
Arjun Ghimire <arjunrazz0714@gmail.com>
2016-04-05 22:57:56 +0545

    Initial Commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,11 +29,11 @@ class PostsController < ApplicationController
 	end
 
     def edit
-    	@post=Post.find(params[:id])
+    	@post=Post.friendly.find(params[:id])
     end
 
     def update
-    	@post=Post.find(params[:id])
+    	@post=Post.friendly.find(params[:id])
     	@post.update_attributes(post_params)
     	redirect_to posts_path
     end
